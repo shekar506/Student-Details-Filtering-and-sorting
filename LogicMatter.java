91eed48ff568cc03ed7268885b59e7cfa63f7caf
Shekar <138869231+shekar506@users.noreply.github.com>
2025-01-07 14:59:09 +0530

    FizzBuzz Coding challenge ( LogicMatter)

---
+++ LogicMatter.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class LogicMatter {
+	
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=100;i++) {
+			if(i%3==0 && i%5==0) {
+				System.out.println("FizzBuzz, ");
+			}
+			else if(i%3==0) {
+				System.out.print("Fizz, ");
+			}
+			else if(i%5==0) {
+				System.out.print("Buzz, ");
+			}
+			else {
+				System.out.print(i+", ");
+			}
+			
+		}
+		
+	}
+
+}
